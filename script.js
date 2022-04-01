f775a3fe2fd20e61c917b33e03d07f2221783c8e
Faud X <77972137+Fuad-X@users.noreply.github.com>
2022-04-01 22:58:26 +0600

    Create script.js
    Script created

---
+++ script.js
@@ -0,0 +1,12 @@
+function openLeftNavigationBar()
+{
+    document.getElementById("leftNav").style.width ="25%";
+    document.getElementById("page").style.width ="75%";
+    document.getElementById("page").style.marginLeft ="25%";
+}
+function closeLeftNavigationBar()
+{
+    document.getElementById("leftNav").style.width ="0%";
+    document.getElementById("page").style.width ="100%";
+    document.getElementById("page").style.marginLeft ="0";
+}
